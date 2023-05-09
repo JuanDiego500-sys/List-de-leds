a73f5e2ff239bdc697a9306ef4a1d9f900e8cf31
JuanDiego500-sys <jdgiraldo98535@umanizales.edu.co>
2023-05-08 22:29:01 -0500

    first version

---
+++ src/main/java/co/edu/umanizales/ListdeledsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListdeledsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListdeledsApplication.class, args);
+    }
+
+}
